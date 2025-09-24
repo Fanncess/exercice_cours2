2ab6447f55000ac14c598086bc67622efb28747c
fjeudy <157497061+Fanncess@users.noreply.github.com>
2025-09-23 23:58:09 -0400

    add

--- Shema2.py
+++ Shema2.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO   # Importe la bibliothèque pour contrôler les GPIO du Raspberry Pi
-import time              # Importe la bibliothèque pour gérer le temps (non utilisé ici, mais souvent utile)
+import time              # Importe la bibliothèque pour gérer le temps (utile pour ajouter des pauses si nécessaire)
 
 GPIO.setmode(GPIO.BCM)   # Définit la numérotation des broches selon le schéma BCM (GPIOxx)
 
@@ -16,14 +16,21 @@ GPIO.setup(PIN_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(GREEN_LED, GPIO.OUT)  # Configure la broche de la LED verte en sortie
 GPIO.setup(RED_LED, GPIO.OUT)    # Configure la broche de la LED rouge en sortie
 
-button = GPIO.input(PIN_BUTTON)  # Lit une fois l’état initial du bouton (mais attention, ce n'est pas mis à jour dans la boucle)
+try:
+    while True:                      # Boucle infinie : exécute en continu le code à l’intérieur
+        button = GPIO.input(PIN_BUTTON)  # Lit l’état du bouton à chaque boucle (mise à jour en continu)
+        
+        if button == GPIO.HIGH:      # Si l’état du bouton est à HIGH (bouton appuyé)
+            GPIO.output(GREEN_LED, GPIO.HIGH)  # Allume la LED verte (envoie un signal électrique)
+            GPIO.output(RED_LED, GPIO.LOW)     # Éteint la LED rouge
+        else:                        # Sinon (bouton non appuyé)
+            GPIO.output(GREEN_LED, GPIO.LOW)   # Éteint la LED verte
+            GPIO.output(RED_LED, GPIO.HIGH)    # Allume la LED rouge
 
-while True:                      # Boucle infinie : exécute en continu le code à l’intérieur
-    if button == GPIO.HIGH:     # Si l’état du bouton est à HIGH (bouton appuyé)
-        GPIO.output(GREEN_LED, GPIO.HIGH)  # Allume la LED verte (envoie un signal électrique)
-        GPIO.output(RED_LED, GPIO.LOW)     # Éteint la LED rouge
-    else:                       # Sinon (bouton non appuyé)
-        GPIO.output(GREEN_LED, GPIO.LOW)   # Éteint la LED verte
-        GPIO.output(RED_LED, GPIO.HIGH)    # Allume la LED rouge
+except KeyboardInterrupt:
+    # Permet de quitter proprement avec Ctrl+C
+    print("\nProgramme interrompu par l’utilisateur.")
 
-GPIO.cleanup()                  # Nettoie les configurations des GPIO (ne sera jamais atteint à cause de la boucle infinie)
+finally:
+    GPIO.cleanup()   # Nettoie les configurations des GPIO (remet les broches dans leur état initial)
+    print("GPIO nettoyés correctement.")
